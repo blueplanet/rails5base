2812cc2db0405d89d8a673e02b18b368b2491b64
blueplanet <erguolinge@gmail.com>
2016-05-12 22:43:28 +0900

    Add generator setting and add i18n

--- config/application.rb
+++ config/application.rb
@@ -2,14 +2,24 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Rails5base
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Asia/Tokyo'
+    config.i18n.available_locales = [:ja]
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.assets false
+      g.view_specs false
+      g.controller_specs false
+      g.helper_specs false
+      g.fixture false
+    end
   end
 end
