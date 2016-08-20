82a361ea5af66e240657697ee4cd08306e15b37b
blueplanet <erguolinge@gmail.com>
2016-08-20 19:02:37 +0900

    add en to locale, change design for index

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module Rails5base
   class Application < Rails::Application
     config.time_zone = 'Asia/Tokyo'
-    config.i18n.available_locales = [:ja]
+    config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
